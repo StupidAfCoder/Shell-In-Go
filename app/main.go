c0739261561671dd85797d0a4768fc9cb4ab926e
StupidAfCoder <malikyash2007@gmail.com>
2025-12-15 21:17:16 +0530

    Forth

--- app/main.go
+++ app/main.go
@@ -5,15 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		command, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err.Error())
+		}
+		command = strings.TrimSpace(command)
+		commandExit := "exit"
+		if command == commandExit {
+			break
 		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
