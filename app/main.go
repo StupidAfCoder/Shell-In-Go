241cad809f0a7e8206565bebfd872d5f695baa72
StupidAfCoder <malikyash2007@gmail.com>
2025-12-15 20:44:13 +0530

    Third

--- app/main.go
+++ app/main.go
@@ -9,10 +9,12 @@ import (
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		log.Fatal(err.Error())
+	for {
+		fmt.Print("$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
