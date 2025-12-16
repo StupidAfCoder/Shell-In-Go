40e55dea614b05637b1fe9b1ccc68a622e450e9a
StupidAfCoder <malikyash2007@gmail.com>
2025-12-16 16:32:39 +0530

    Sixth

--- app/main.go
+++ app/main.go
@@ -8,6 +8,46 @@ import (
 	"strings"
 )
 
+var allCommands = []string{"echo", "exit", "type"}
+
+func checkCommand(command string) (string, string) {
+	cmd, param, found := strings.Cut(command, " ")
+	if !found {
+		return command[:len(command)-1] + ": command not found", ""
+	}
+	switch cmd {
+	case "echo":
+		return cmd, param
+	case "type":
+		return cmd, param
+	default:
+		return command[:len(command)-1] + ": command not found", ""
+	}
+}
+
+func executeCommand(command string, param string) {
+	if param == "" {
+		fmt.Println(command)
+	}
+	switch command {
+	case "echo":
+		fmt.Print(param)
+	case "type":
+		actualCommand := false
+		for _, v := range allCommands {
+			if param == v {
+				actualCommand = true
+				break
+			}
+		}
+		if actualCommand {
+			fmt.Printf("%s is a shell builtin\n", param)
+		} else {
+			fmt.Printf("%s: not found\n", param)
+		}
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -21,11 +61,7 @@ func main() {
 		if command2 == commandExit {
 			break
 		}
-		switch {
-		case command[0:4] == "echo":
-			fmt.Println(command2[5:])
-		default:
-			fmt.Println(command[:len(command)-1] + ": command not found")
-		}
+		checkedCommand, param := checkCommand(command2)
+		executeCommand(checkedCommand, param)
 	}
 }
