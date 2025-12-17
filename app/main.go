6beb593da666c65782565122547685b139dae91d
StupidAfCoder <malikyash2007@gmail.com>
2025-12-18 00:21:30 +0530

    "Eight"

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"slices"
 	"strings"
@@ -13,10 +14,53 @@ import (
 var allCommands = []string{"echo", "exit", "type"}
 var PATH = os.Getenv("PATH")
 
+func runCommand(path string, args []string) error {
+
+	if args == nil {
+		cmd := exec.Command(path)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		return cmd.Run()
+	}
+
+	cmd := exec.Command(path, args[:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func checkIfExecutableExists(exec string) (string, bool) {
+	pathSlice := strings.Split(PATH, ":")
+	for _, path := range pathSlice {
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			continue
+		}
+		for _, entry := range entries {
+			if entry.Name() == exec {
+				filePath := filepath.Join(path, exec)
+				info, err := os.Stat(filePath)
+				if err != nil {
+					continue
+				}
+				isExecutable := info.Mode()&0111 != 0
+				if isExecutable {
+					return filePath, true
+				}
+			}
+		}
+	}
+	return "", false
+}
+
 func checkCommand(command string) (string, string) {
 	cmd, param, found := strings.Cut(command, " ")
 	if !found {
-		return command[:] + ": command not found", ""
+		return command, ""
 	}
 	switch cmd {
 	case "echo":
@@ -24,14 +68,11 @@ func checkCommand(command string) (string, string) {
 	case "type":
 		return cmd, param
 	default:
-		return command[:] + ": command not found", ""
+		return cmd, param
 	}
 }
 
 func executeCommand(command string, param string) {
-	if param == "" {
-		fmt.Println(command)
-	}
 	switch command {
 	case "echo":
 		fmt.Println(param)
@@ -41,38 +82,31 @@ func executeCommand(command string, param string) {
 			fmt.Printf("%s is a shell builtin\n", param)
 			break
 		}
-		directoryCommand := false
-		foundPath := ""
-		pathSlice := strings.Split(PATH, ":")
-		for _, path := range pathSlice {
-			entries, err := os.ReadDir(path)
-			if err != nil {
-				continue
-			}
-			for _, entry := range entries {
-				if entry.Name() == param {
-					filePath := filepath.Join(path, param)
-					info, err := os.Stat(filePath)
-					if err != nil {
-						continue
-					}
-					isExecutable := info.Mode()&0111 != 0
-					if isExecutable {
-						foundPath = filePath
-						directoryCommand = true
-						break
-					}
-				}
-			}
-			if directoryCommand {
-				break
-			}
-		}
+		foundPath, directoryCommand := checkIfExecutableExists(param)
 		if directoryCommand {
 			fmt.Printf("%s is %s\n", param, foundPath)
 			break
 		}
 		fmt.Printf("%s: not found\n", param)
+	default:
+		foundPath, directoryCommand := checkIfExecutableExists(command)
+		if directoryCommand {
+			param = strings.TrimSpace(param)
+			if len(param) == 0 {
+				err := runCommand(foundPath, nil)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			} else {
+				splitParam := strings.Split(param, " ")
+				err := runCommand(foundPath, splitParam)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+		} else {
+			fmt.Println(command[:] + ": command not found" + foundPath)
+		}
 	}
 }
 
